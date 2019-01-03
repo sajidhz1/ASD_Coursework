a3e4c29809de7d0869edcc910aba721fbcf4fbe9
sajidhz1 <sajidhzazahir@gmail.com>
2019-01-03 19:11:58 +0530

    inital empty screens

---
+++ app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/SpendingFragment.java
@@ -0,0 +1,23 @@
+package expensetracker.iit.com.expensetracker.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import expensetracker.iit.com.expensetracker.R;
+
+public class SpendingFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.spending_fragment, container, false);
+    }
+}
