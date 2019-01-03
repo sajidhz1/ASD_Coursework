0c8a8db1dfca1c52f4ac57835192b251c0476bed
sajidhz1 <sajidhzazahir@gmail.com>
2019-01-03 21:36:53 +0530

    add sort toolbar

--- app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/TransactionsFragment.java
+++ app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/TransactionsFragment.java
@@ -1,14 +1,21 @@
 package expensetracker.iit.com.expensetracker.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import expensetracker.iit.com.expensetracker.R;
 
 public class TransactionsFragment extends Fragment {
+
+    private ListView transactionsList;
 
     public static TransactionsFragment newInstance() {
         TransactionsFragment fragment = new TransactionsFragment();
@@ -24,5 +31,61 @@ public class TransactionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.transactions_fragment, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        String[] textString = {"Item1", "Item2", "Item3", "Item4"};
+        int[] drawableIds = {R.drawable.ic_notifications_black_24dp, R.drawable.ic_notifications_black_24dp, R.drawable.ic_notifications_black_24dp, R.drawable.ic_notifications_black_24dp};
+
+        TransactionAdapter adapter = new TransactionAdapter(this.getContext(),  textString, drawableIds);
+        transactionsList = (ListView)getView().findViewById(R.id.transactionsList);
+        transactionsList.setAdapter(adapter);
+    }
+
+    public class TransactionAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private String[]  Title;
+        private int[] imge;
+
+        public TransactionAdapter(Context context, String[] text1, int[] imageIds) {
+            mContext = context;
+            Title = text1;
+            imge = imageIds;
+
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return Title.length;
+        }
+
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row;
+            row = inflater.inflate(R.layout.transactions_row, parent, false);
+            TextView title;
+            ImageView i1;
+            i1 = (ImageView) row.findViewById(R.id.imgIcon);
+            title = (TextView) row.findViewById(R.id.txtTitle);
+            title.setText(Title[position]);
+            i1.setImageResource(imge[position]);
+
+            return (row);
+        }
     }
 }
