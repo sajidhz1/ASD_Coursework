aa61af596df9b0d302d0f3f590da92af37e634cc
sajidhz1 <sajidhzazahir@gmail.com>
2019-01-03 23:36:52 +0530

    add empty adapters to categries and spendings page

--- app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/CategoriesFragment.java
+++ app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/CategoriesFragment.java
@@ -1,11 +1,14 @@
 package expensetracker.iit.com.expensetracker.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import expensetracker.iit.com.expensetracker.R;
 
@@ -29,5 +32,49 @@ public class CategoriesFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.categories_fragment, container, false);
+    }
+
+    public class CategoriesAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private String[]  Title;
+        private int[] imge;
+
+        public CategoriesAdapter(Context context, String[] text1, int[] imageIds) {
+            mContext = context;
+            Title = text1;
+            imge = imageIds;
+
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return Title.length;
+        }
+
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row;
+            row = inflater.inflate(R.layout.transactions_row, parent, false);
+            TextView title;
+            ImageView i1;
+            i1 = (ImageView) row.findViewById(R.id.imgIcon);
+            title = (TextView) row.findViewById(R.id.txtTitle);
+            title.setText(Title[position]);
+            i1.setImageResource(imge[position]);
+
+            return (row);
+        }
     }
 }
--- app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/SpendingFragment.java
+++ app/src/main/java/expensetracker/iit/com/expensetracker/Fragments/SpendingFragment.java
@@ -1,10 +1,14 @@
 package expensetracker.iit.com.expensetracker.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import expensetracker.iit.com.expensetracker.R;
 
@@ -24,5 +28,49 @@ public class SpendingFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.spending_fragment, container, false);
+    }
+
+    public class SpendingsAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private String[]  Title;
+        private int[] imge;
+
+        public SpendingsAdapter(Context context, String[] text1, int[] imageIds) {
+            mContext = context;
+            Title = text1;
+            imge = imageIds;
+
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return Title.length;
+        }
+
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row;
+            row = inflater.inflate(R.layout.transactions_row, parent, false);
+            TextView title;
+            ImageView i1;
+            i1 = (ImageView) row.findViewById(R.id.imgIcon);
+            title = (TextView) row.findViewById(R.id.txtTitle);
+            title.setText(Title[position]);
+            i1.setImageResource(imge[position]);
+
+            return (row);
+        }
     }
 }
