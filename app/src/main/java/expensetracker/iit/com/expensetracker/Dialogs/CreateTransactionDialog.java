70baf308f74c0d87be3b378519069f5385164e7c
sajidhz1 <sajidhzazahir@gmail.com>
2019-01-05 21:24:46 +0530

    update add transaction dialog

--- app/src/main/java/expensetracker/iit/com/expensetracker/Dialogs/CreateTransactionDialog.java
+++ app/src/main/java/expensetracker/iit/com/expensetracker/Dialogs/CreateTransactionDialog.java
@@ -1,16 +1,26 @@
 package expensetracker.iit.com.expensetracker.Dialogs;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import expensetracker.iit.com.expensetracker.R;
 
 public class CreateTransactionDialog extends Dialog {
+
+    EditText dateEditText;
+    final Calendar calendar = Calendar.getInstance();
 
     public CreateTransactionDialog(@NonNull Context context) {
         super(context, R.style.full_screen_dialog);
@@ -22,6 +32,33 @@ public class CreateTransactionDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_transaction_dialog);
+
+        dateEditText = (EditText) findViewById(R.id.date);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+
+        dateEditText.setOnClickListener((View v) -> {
+            new DatePickerDialog(getContext(), date, calendar
+                    .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)).show();
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateEditText.setText(sdf.format(calendar.getTime()));
     }
 
     @Override
