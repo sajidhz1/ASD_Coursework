faa228588e97fe5b9a941ad9c371547ead287d06
sachilaRanawaka <sachila@duosoftware.com>
2019-01-10 01:12:28 +0530

    add the registration page

--- app/src/main/java/expensetracker/iit/com/expensetracker/PinActivity.java
+++ app/src/main/java/expensetracker/iit/com/expensetracker/PinActivity.java
@@ -1,12 +1,30 @@
 package expensetracker.iit.com.expensetracker;
 
 import android.app.Activity;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.util.List;
+
+import expensetracker.iit.com.expensetracker.Model.Transaction;
+import expensetracker.iit.com.expensetracker.Model.User;
+import expensetracker.iit.com.expensetracker.ViewModel.UserViewModel;
 
 
-public class PinActivity extends Activity {
+public class PinActivity extends AppCompatActivity {
+
+    EditText userName, pinNo;
+    Button saveButton;
+    private UserViewModel mUserViewModel;
+
 
     public static PinActivity newInstance() {
         PinActivity fragment = new PinActivity();
@@ -16,11 +34,32 @@ public class PinActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pin_fragment);
-    }
+
+        mUserViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        setContentView(R.layout.user_registration);
+
+        pinNo = (EditText) findViewById(R.id.pinNo);
+        userName = (EditText) findViewById(R.id.regUserName);
+        saveButton =  findViewById(R.id.regSubmit);
+
+        saveButton.setOnClickListener((View v) -> {
+            User user = new User(userName.getText().toString(), Integer.parseInt(pinNo.getText().toString()));
+            mUserViewModel.insert(user);
+
+            LiveData<List<User>> userArr = mUserViewModel.getAll();
+            Intent i = new Intent(PinActivity.this, MainActivity.class);
+            startActivity(i);
+        });
+
+        }
 
     public void pinSubmit(View v){
         Intent i = new Intent(PinActivity.this, MainActivity.class);
         startActivity(i);
+    }
+
+    public void registerUser(View v){
+
+
     }
 }
---
+++ app/src/main/java/expensetracker/iit/com/expensetracker/ViewModel/UserViewModel.java
@@ -0,0 +1,27 @@
+package expensetracker.iit.com.expensetracker.ViewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import expensetracker.iit.com.expensetracker.Model.User;
+import expensetracker.iit.com.expensetracker.Repository.UserRepository;
+
+public class UserViewModel extends AndroidViewModel {
+
+    private UserRepository mRepository;
+
+    public void insert(User user) { mRepository.insert(user); }
+
+    public void delete(User user) { mRepository.delete(user); }
+
+    public LiveData<List<User>> getAll() { return mRepository.getAllUsers(); }
+
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new UserRepository(application);
+    }
+}
